bd3d48e75e0fe76031903bf7256c79212ee4718b
e41c <ericgrigor@gmail.com>
2023-11-22 12:55:13 -0500

    second commit

--- App.js
+++ App.js
@@ -3,12 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AlbumListScreen from './src/AlbumListScreen';
 import AlbumDetailsScreen from './src/AlbumDetailsScreen';
+import { StyleSheet } from 'react-native';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer style={styles.container}>
       <Stack.Navigator initialRouteName="AlbumList">
         <Stack.Screen name="AlbumList" component={AlbumListScreen} />
         <Stack.Screen name="AlbumDetails" component={AlbumDetailsScreen} />
@@ -16,5 +17,12 @@ const App = () => {
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue', 
+  },
+});
 
 export default App;
